15ce4c97f96f785a8e4daf4b4d9a930ac4ec0451
adar-apiiro <144586914+adar-apiiro@users.noreply.github.com>
2023-11-14 16:21:22 +0200

    Update Examples.py

--- ZoomInfo/Examples.py
+++ ZoomInfo/Examples.py
@@ -3,5 +3,4 @@ from langchain import PromptTemplate, LLMChain, Anthropic
 prompt = PromptTemplate(template=template_no_overlap, input_variables=["topics"])
 llm = Anthropic(temperature=1, model="claude-v1.3")
 llm_chain = LLMChain(prompt=prompt, llm=llm)
-
 llm_name_chain = LLMChain(prompt=prompt, llm=llm)
